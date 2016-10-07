00ab90feea8a16248a88fc53cd4026d522f328dd
k <k@debian>
2016-10-08 02:20:28 +1030

    Passes level 4.

--- 2016CP2Assignment2/main.cpp
+++ 2016CP2Assignment2/main.cpp
@@ -10,6 +10,9 @@
 #include <iostream>
 #include <unistd.h>
 
+//need this to convert strings into lowercase
+#include <algorithm>
+
 using namespace std;
 bool matchWhole(string, string);
 bool matchPrefix(string, string);
@@ -23,9 +26,10 @@ int main(int argc, char* argv[]) {
         WHOLE, PREFIX, SUFFIX, ANYWHERE, EMBEDDED
     } mode = WHOLE;
     bool reverse_match = false;
+    bool case_sensitive = true;
 
     int c;
-    while ((c = getopt(argc, argv, ":wpsaev")) != -1) {
+    while ((c = getopt(argc, argv, ":wpsaevi")) != -1) {
         switch (c) {
             case 'w': // pattern matches whole word
                 mode = WHOLE;
@@ -45,6 +49,9 @@ int main(int argc, char* argv[]) {
             case 'v': // reverse sense of match
                 reverse_match = true;
                 break;
+            case 'i': // disregard case of letters
+                case_sensitive = false;
+                break;
         }
     }
     argc -= optind;
@@ -52,58 +59,69 @@ int main(int argc, char* argv[]) {
 
     string pattern = argv[0];
 
-    string word;
+    //convert pattern to lowercase if -i is used
+    if (!case_sensitive) {
+        transform(pattern.begin(), pattern.end(), pattern.begin(), ::tolower);
+    }
+
+    string word, oldWord;
     int matches = 0;
     bool found = false;
-    
+
     //loops for each word
     while (cin >> word) {
+        
+        oldWord = word;
+        //convert word to lowercase if -i is used
+        if (!case_sensitive) {
+            transform(word.begin(), word.end(), word.begin(), ::tolower);
+        }
 
         //divide and conquer - each mode has its own function.
         switch (mode) {
             case WHOLE:
-                if(matchWhole(pattern, word)){
+                if (matchWhole(pattern, word)) {
                     found = true;
                     matches++;
                 }
                 break;
             case PREFIX:
-                if(matchPrefix(pattern, word)){
+                if (matchPrefix(pattern, word)) {
                     found = true;
                     matches++;
                 }
                 break;
             case ANYWHERE:
-                if(matchAnywhere(pattern, word)){
+                if (matchAnywhere(pattern, word)) {
                     found = true;
                     matches++;
                 }
                 break;
             case SUFFIX:
-                if(matchSuffix(pattern, word)){
+                if (matchSuffix(pattern, word)) {
                     found = true;
                     matches++;
                 }
                 break;
             case EMBEDDED:
-                if(matchEmbedded(pattern, word)){
+                if (matchEmbedded(pattern, word)) {
                     found = true;
                     matches++;
                 }
                 break;
         }
-        
-        if (reverse_match){
+
+        if (reverse_match) {
             //cout << "reverse matching" << endl;
-            if(found) matches--;
-            else matches ++;
+            if (found) matches--;
+            else matches++;
             found = !found;
         }
-        
-        if (found){
-            cout << word << endl;
+
+        if (found) {
+            cout << oldWord << endl;
         }
-        
+
         found = false;
 
     }
@@ -122,8 +140,7 @@ int main(int argc, char* argv[]) {
 bool matchWhole(string pattern, string word) {
     if (pattern == word) {
         return true;
-    }
-    else return false;
+    } else return false;
 }
 
 /**
@@ -132,10 +149,9 @@ bool matchWhole(string pattern, string word) {
  * @param word
  */
 bool matchPrefix(string pattern, string word) {
-    if(word.find(pattern) == 0){
+    if (word.find(pattern) == 0) {
         return true;
-    }
-    else return false;
+    } else return false;
 }
 
 /**
@@ -144,10 +160,9 @@ bool matchPrefix(string pattern, string word) {
  * @param word
  */
 bool matchAnywhere(string pattern, string word) {
-    if(word.find(pattern) != string::npos){
+    if (word.find(pattern) != string::npos) {
         return true;
-    }
-    else return false;
+    } else return false;
 }
 
 /**
@@ -156,12 +171,11 @@ bool matchAnywhere(string pattern, string word) {
  * @param word
  */
 bool matchSuffix(string pattern, string word) {
-    
+
     int biggestIndex = word.size() - pattern.size();
-    if(word.find(pattern) == biggestIndex && biggestIndex >=0){
+    if (word.find(pattern) == biggestIndex && biggestIndex >= 0) {
         return true;
-    }
-    else return false;
+    } else return false;
 }
 
 /**
@@ -170,8 +184,7 @@ bool matchSuffix(string pattern, string word) {
  * @param word
  */
 bool matchEmbedded(string pattern, string word) {
-    if(pattern.find(word) != string::npos){
+    if (pattern.find(word) != string::npos) {
         return true;
-    }
-    else return false;
+    } else return false;
 }
